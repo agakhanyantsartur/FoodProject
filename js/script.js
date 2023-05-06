f0d57a195788eb6a03b91feeae4aec89934d3ba3
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-05-06 23:36:41 +0500

    optimised the work of modals

--- js/script.js
+++ js/script.js
@@ -112,14 +112,18 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTrigger = document.querySelectorAll('[data-modal]');
     const modal = document.querySelector('.modal');
     const modalCloseBtn = document.querySelector('[data-close]');
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
     
     //Так как нам нужно повесить событие на несколько кнопок, мы прописали querySelectorAll и теперь должны перебрать их через forEach
     modalTrigger.forEach (btn => {
-        btn.addEventListener('click', () =>{
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden'; // Делаем так, чтобы когда открыто модальное окно, нельзя было скролить
-        });
+        btn.addEventListener('click', openModal);
+             // Делаем так, чтобы когда открыто модальное окно, нельзя было скролить
     });
 
     function closeModal() {
@@ -128,7 +132,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    modalCloseBtn.addEventListener('click', closeModal);
+    modalCloseBtn.addEventListener('click', closeModal); 
 
     //Делаем так, чтобы когда пользователь кликал на подложку во время откытого модального окна, окно закрывалось
     modal.addEventListener('click', (e) => {
@@ -136,6 +140,7 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
     //Делаем так, чтобы когда пользователь нажимал клавишу Esc, модальное окно закрывалось
     document.addEventListener('keydown', (e) => {
         if (e.code === 'Escape' && modal.classList.contains('show')) { //Делам так, чтобы при нажатии на Esc, даже когда модальное окно было закрыто, функция закрытия модального окна срабатывала
@@ -143,4 +148,16 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //Также делаем так, чтобы модальное окно всплывало через 10-15 сек на сайте
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    //Показываем пользователю модальное окно, если он долистал до конца
+    window.addEventListener('scroll', showModalByScroll);
 });
