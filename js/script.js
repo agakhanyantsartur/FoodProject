dd7eaf01ba52a6d9fcb9956c32123e342630647e
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-06-24 08:23:59 +0500

    Implemented getting the data from user in JSON

--- js/script.js
+++ js/script.js
@@ -263,14 +263,26 @@ window.addEventListener('DOMContentLoaded', () => {
             request.open('POST', 'js/server.php');
 
             // request.setRequestHeader('Content-type', 'multipart/form-data'); // когда мы используем связку XMLHttpRequest + formData нам не нужно устанавливать заголовок, так как он устанавливается автоматически
+            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
-            request.send(formData);
+            const object = {};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
 
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
                     statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 2000)
                 } else {
                     statusMessage.textContent = message.failure;
                 }
