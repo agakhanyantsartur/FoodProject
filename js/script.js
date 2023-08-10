a08d9b2b7ac77ce28bccbefa31e3af107f526a83
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-08-10 23:04:59 +0500

    getResource async/await func was replaces with axios just for diversity and practice

--- js/script.js
+++ js/script.js
@@ -210,9 +210,16 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     //Делаем автоматическое отображение элементов на странице с файла db.json
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         });
