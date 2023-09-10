7b6fd4b41d4fdbf370cb425c47faadf4a44235b4
agakhanyantsartur <agakhanyantsartur@gmail.com>
2023-09-10 23:34:45 +0500

    .

--- js/modules/calculator.js
+++ js/modules/calculator.js
@@ -3,19 +3,7 @@ function calculator() {
 
   let sex, height, weight, age, ratio;
 
-  if (localStorage.getItem('sex')) {
-    sex = localStorage.getItem('sex');
-  } else {
-    sex = 'female';
-    localStorage.setItem('sex', 'female');
-  }
-
-  if (localStorage.getItem('ratio')) {
-    ratio = localStorage.getItem('ratio');
-  } else {
-    ratio = 1.375;
-    localStorage.setItem('ratio', 1.375);
-  }
+  
 
   function initLocalSettings(selector, activeClass) {
     const elements = document.querySelectorAll(selector);
